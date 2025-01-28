479cb5db55346718d4e1c496056ecdc8e133485d
Luis-Arguelles <luisarguelles2003@gmail.com>
2025-01-28 00:00:23 -0600

    Created EventModel, EventController, the resource routing and trying to solve CORS error

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('test', function () {
+    return 'Test route is working!';
+});
+
+$routes->resource('events', ['controller' => 'EventController', 'filter' => 'cors']);
---
+++ app/Filters/CorsFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class CorsFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        header('Access-Control-Max-Age: 86400');
+
+        // Handle preflight (OPTIONS) requests
+        if ($request->getMethod() === 'options') {
+            exit;
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Not needed for CORS
+    }
+}
